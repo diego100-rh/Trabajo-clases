6c43b28bb15c60a1eb20af25536bf8e0368aae96
diegoo <d.rifo01@ufromail.cl>
2026-08-31 20:13:58 -0400

    Prueba 02 "hola mundo" + apellido

--- src/HolaMundo.java
+++ src/HolaMundo.java
@@ -1,5 +1,7 @@
 public class HolaMundo {
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!!");
+
+        System.out.println("Soy Diego Rifo ");
     }
 }// DE DIEGO RIFO
